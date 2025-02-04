73aa0ba046143a6c7e92fb3dfed21e0fac95252b
nipuana <nipuanarimal@gmail.com>
2025-02-04 13:58:00 +0545

    made changes to orientation and added a api folder for adding controller apis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import Body from "./components/Body";
+import Header from './components/Landing/Header';
+import Body from "./components/Landing/Body";
 import './App.css';
 
 function App() {
--- src/components/Landing/Body.jsx
+++ src/components/Landing/Body.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import "../css/Body.css";
+import "../../css/Body.css";
 
-const FarmerLandingPage = () => {
+const Body = () => {
   return (
     <div className="container">
-      {/* Left Text Section */}
+    
       <div className="text-section">
         <h1>
-          All the <br /> products <br />
+          All the necessary <br /> products <br />
           <span className="highlight">for farmers</span>
         </h1>
         <p>Everything that you can ever hope of</p>
         <button className="register-button">Register</button>
       </div>
 
-      {/* Right Illustration */}
+      
       <div className="image-section">
         <img
-          src="./images/m1_img.png"
+          src="FarmHelp_Frontend\src\images\m1_img.png"
           alt="Farmer illustration"
         />
       </div>
@@ -25,4 +25,4 @@ const FarmerLandingPage = () => {
   );
 };
 
-export default FarmerLandingPage;
+export default Body;
